4b4bb9d1dc3eccc6109e0f3b917b6082d35c44c6
AlexQ28 <aquezada010@gmail.com>
2020-04-23 11:08:31 -0500

    first commit

---
+++ datatype.py
@@ -0,0 +1,50 @@
+
+#Strings
+print("Hello world")
+print('Hello World')
+print('''Hello World''')
+print("""Hello World""")
+
+print (type("Hello World"))
+
+print("Bye" + "World")
+
+#Numbers
+
+#Integer
+print(30)
+
+#Float es decimal 
+print(30.4)
+
+#Boolean 
+#Palabras Reservadas
+True
+False
+
+#List
+#Lista de Datos
+[10, 20, 30, 55]
+['Hello', 'Bye', "Adios"]
+[10, "Hello", True, 10.1]
+[] #Va vacio
+
+
+#Tuples
+#La lista ya no puede cambiar en Tuples
+(10, 20, 30, 55) #Se define con ()
+()#Va vacio 
+
+# Diccionarios
+
+print(type({
+  "name":"Jhon",
+  "lastname":"Ray",
+  "nickname":"Fast"
+
+}
+))
+
+None
+
+
---
+++ helloworld.py
@@ -0,0 +1,5 @@
+#Este codigo solo imprime 
+#un mensaje por pantalla 
+print ("hello world")
+
+
