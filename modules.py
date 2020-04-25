3991992517cde4828f56e6eaeb730c41f6b95c2a
AlexQ28 <aquezada010@gmail.com>
2020-04-25 10:19:40 -0500

    5 commit

---
+++ modules.py
@@ -0,0 +1,23 @@
+# own modules
+# thirdy party modules
+# python modules
+
+# import datetime 
+# print(datetime.date.today())
+
+# print(datetime.timedelta(minutes=100))
+
+# from datetime import timedelta, date
+# print(date.today())
+
+# import fmath
+
+# fmath.add(1, 2)
+
+# fmath.substract(1, 2)
+
+from fmath import add, substract
+
+substract(1, 2)
+add(1, 2)
+
